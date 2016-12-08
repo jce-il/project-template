7882eb4eb09afb7105bc004baa69c2c50d36f909
reutnagar <salreut1@gmail.com>
2016-12-08 16:54:55 +0200

    third run in heroku

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
-const express = require('express');
-const app = express();
+//const express = require('express');
+//const app = express();
 
-app.listen(443, function () {
-    console.log('listening on 3000')
-})
+//app.listen(443, function () {
+//    console.log('listening on 3000')
+//})
 
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/src/Admin/index.html')
-})
+//app.get('/', function (req, res) {
+//    res.sendFile(__dirname + '/src/Admin/index.html')
+//})
+
+const server = express()
+  .use((req, res) => res.sendFile(__dirname + '/src/Admin/index.html'))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
