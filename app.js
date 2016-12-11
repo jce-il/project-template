9a2aae3ecb73418739e7ad59a33e5ac1e9ccdd23
reutnagar <salreut1@gmail.com>
2016-12-11 14:42:25 +0200

    added DB  connection
    test DB is: "TestDB"

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
+//include .js files
+var data = require('./src/server/database.js');
+
+//
 var express = require('express');
 var app = express();
 
-app.use(express.static('src/Admin'));
+//add all linked files in index.html
+app.use(express.static('src/admin'));
 
+//send Admin/index.html + all linked files
 app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/src/Admin/index.html');
+    res.sendFile(__dirname + '/src/admin/index.html');
 });
 
+//listen in port 3000. in heroku.com it should be 'PORT': an enviroment variable.
 app.listen(3000, function () {
     console.log('listening on 3000')
 })
 
-
+//testing
+console.log(data.DB.getProp1());
