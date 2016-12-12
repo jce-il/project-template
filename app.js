10dca6457e4eb03a074e2e3ea36c359f9c8a131d
shoshanasi <shenanigans995@gmail.com>
2016-12-12 15:12:48 +0200

    Revert "added DB  connection"
    This reverts commit 9a2aae3ecb73418739e7ad59a33e5ac1e9ccdd23.

--- app.js
+++ app.js
@@ -1,22 +1,14 @@
-//include .js files
-var data = require('./src/server/database.js');
-
-//
 var express = require('express');
 var app = express();
 
-//add all linked files in index.html
-app.use(express.static('src/admin'));
+app.use(express.static('src/Admin'));
 
-//send Admin/index.html + all linked files
 app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/src/admin/index.html');
+    res.sendFile(__dirname + '/src/Admin/index.html');
 });
 
-//listen in port 3000. in heroku.com it should be 'PORT': an enviroment variable.
 app.listen(3000, function () {
     console.log('listening on 3000')
 })
 
-//testing
-console.log(data.DB.getProp1());
+
