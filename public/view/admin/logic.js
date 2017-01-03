daa4c3db15b892b1b3e4859f3fd1285c1f8bd627
reutnagar <salreut1@gmail.com>
2017-01-03 16:34:41 +0200

    remove dist from .gitignore

--- public/view/admin/logic.js
+++ public/view/admin/logic.js
@@ -1,3 +1,4 @@
+/*
 var databaseUrl = "mydb"; // "username:password@example.com/mydb"
 var collections = ["users", "reports"]
 var db = require("mongojs").connect(databaseUrl, collections);
@@ -10,3 +11,4 @@ for(var i = 0; i< collections.length; i++){
     print('Collection: ' + collections[i]); // print the name of each collection
     db.getCollection(collections[i]).find().forEach(printjson); //and then print the json of each of its elements
 }
+*/
