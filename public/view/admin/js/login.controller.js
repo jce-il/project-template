e0469532d591e7cafb8c92d364860b04c0ad2708
tovakl <klugmantova@gmail.com>
2017-01-08 22:23:51 +0200

    Update login.controller.js

--- public/view/admin/js/login.controller.js
+++ public/view/admin/js/login.controller.js
@@ -1,33 +1,51 @@
-(function () {
-    'use strict';
+function signInController($scope, $routeParams, $http) {
 
-    angular
-        .module('app')
-        .controller('LoginController', LoginController);
+    $scope.session = {};
 
-    LoginController.$inject = ['$location', 'AuthenticationService', 'FlashService'];
-    function LoginController($location, AuthenticationService, FlashService) {
-        var vm = this;
+    $scope.signing = function (user) {
+        console.log('in sign in');
 
-        vm.login = login;
+        if ($scope.inForm.$valid) {
 
-        (function initController() {
-            // reset login status
-            AuthenticationService.ClearCredentials();
-        })();
+            console.log('after validation');
+            $scope.user = angular.copy(user);
 
-        function login() {
-            vm.dataLoading = true;
-            AuthenticationService.Login(vm.username, vm.password, function (response) {
-                if (response.success) {
-                    AuthenticationService.SetCredentials(vm.username, vm.password);
-                    $location.path('/');
-                } else {
-                    FlashService.Error(response.message);
-                    vm.dataLoading = false;
-                }
-            });
-        };
+            $http.post('/userManage/signIn', $scope.user)
+                .success(function (response) {
+
+                    $scope.result = response;
+                    if (response.in == true) {
+
+                        $scope.session.user = response.user;
+
+                        alert(response.msg);
+
+                        document.getElementById("name").textContent = $scope.session.user.userName + " | ";
+
+
+                        console.log('after login');
+                        console.log($scope.session.user);
+
+
+                        window.location.replace('#/home');
+
+
+                    }
+                    else if(response.user == null)
+                    {
+                        alert(response.msg);
+                        window.location.replace('#/home');
+                    }
+                    else
+                        alert(response.msg);
+
+                    
+                })
+                .error(function (error) {
+                    console.log('Error: ' + error);
+                    console.log('HTTP: ' + $http);
+
+                });
+        }
     }
-
-})();
+}
