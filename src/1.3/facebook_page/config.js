4b54986e02fb3e79b37329b330892f806947e1ad
avicohen89 <avic132@gmail.com>
2017-05-10 11:17:02 +0300

    Version 1.5.7

--- src/1.3/facebook_page/config.js
+++
@@ -1,9 +0,0 @@
-let config = 
-{
-    apiKey: "AIzaSyBU9GCk3-4FVsBJp2Etpy2pmU2TTgR7U6c",
-    authDomain: "test-9d3f7.firebaseapp.com",
-    databaseURL: "https://test-9d3f7.firebaseio.com",
-    storageBucket: "test-9d3f7.appspot.com",
-    messagingSenderId: "692751540092"
-}; // API keys configuration
-firebase.initializeApp(config); // sending API key to server for verification 
