34dda8a7e04ddfb617d00977d8acd2f8d2a45600
alexlvz <termister2000@gmail.com>
2018-04-23 20:35:01 +0300

    uploaded DB_IO

---
+++ Tests/database_IO_example/src/environments/environment.ts
@@ -0,0 +1,16 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyA2gFHnDMO4VLwrc4f3SGaK7q6TLV6F3d0",
+    authDomain: "dbtests-4c8c6.firebaseapp.com",
+    databaseURL: "https://dbtests-4c8c6.firebaseio.com",
+    projectId: "dbtests-4c8c6",
+    storageBucket: "dbtests-4c8c6.appspot.com",
+    messagingSenderId: "187370169023"
+  }
+};
