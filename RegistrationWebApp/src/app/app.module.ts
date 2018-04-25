c186cb50523be576614b79216151fa7a42a40e8d
Adi Halali <adihalali@Adis-MacBook-Pro.local>
2018-04-25 18:47:41 +0300

    added login screen and bootstrap

--- RegistrationWebApp/src/app/app.module.ts
+++ RegistrationWebApp/src/app/app.module.ts
@@ -1,20 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
 import { RegistrationFormComponent } from './registration-form/registration-form.component';
-
+import { LoginScreenComponent } from './login-screen/login-screen.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegistrationFormComponent
+    RegistrationFormComponent,
+    LoginScreenComponent,
+    LoginScreenComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
---
+++ RegistrationWebApp/src/app/login-screen/login-screen.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-login-screen',
+  templateUrl: './login-screen.component.html',
+  styleUrls: ['./login-screen.component.css']
+})
+export class LoginScreenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
