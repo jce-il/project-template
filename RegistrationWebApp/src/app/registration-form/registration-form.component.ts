ab50ef7198bd03acd0bfd1748e6607fa1824f491
Itay-Hefetz <hefetz99@gmail.com>
2018-04-27 00:51:41 +0300

    a

--- RegistrationWebApp/src/app/registration-form/registration-form.component.ts
+++ RegistrationWebApp/src/app/registration-form/registration-form.component.ts
@@ -4,6 +4,8 @@ import { Component, OnInit } from '@angular/core';
 import {DatabaseService} from '../services/database.service';
 import { AngularFirestore } from'angularfire2/firestore';
 import { RouterLink, Router } from '@angular/router';
+import { FormsModule, FormGroup,FormControl, FormBuilder ,Validators,ReactiveFormsModule  } from '@angular/forms';
+
 
 @Component({
   selector: 'app-registration-form',
@@ -13,6 +15,23 @@ import { RouterLink, Router } from '@angular/router';
 export class RegistrationFormComponent{
   types = ['student', 'teacher', 'checker', 'manager'];
   user = new User(this.types[0], '', '', '', '', '')
+  userform: FormGroup;
+
+  ngOnInit(): void {
+    this.userform = new FormGroup({
+      'firstName': new FormControl(this.user.firstName, [
+        Validators.required,
+        Validators.minLength(2)
+      ]),
+      'lastName': new FormControl(this.user.lastName, [
+        Validators.required
+      ]) 
+    });
+  }
+
+  get firstname() { return this.userform.get('firstName'); }
+  get lastname() { return this.userform.get('lastName'); }
+
 
   submitted = false;
 
