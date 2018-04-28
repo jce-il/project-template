00749106ce5fecb67f12d92519ae2f6cca4571b7
Itay-Hefetz <hefetz99@gmail.com>
2018-04-28 14:29:28 +0300

    registertion validation form

--- RegistrationWebApp/src/app/registration-form/registration-form.component.ts
+++ RegistrationWebApp/src/app/registration-form/registration-form.component.ts
@@ -14,23 +14,40 @@ import { FormsModule, FormGroup,FormControl, FormBuilder ,Validators,ReactiveFor
 })
 export class RegistrationFormComponent{
   types = ['student', 'teacher', 'checker', 'manager'];
-  user = new User(this.types[0], '', '', '', '', '')
+  user = new User(this.types[0])
   userform: FormGroup;
+  emailPattern = "^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$";
 
   ngOnInit(): void {
     this.userform = new FormGroup({
-      'firstName': new FormControl(this.user.firstName, [
+      'firstname': new FormControl(this.user.firstName, [
         Validators.required,
-        Validators.minLength(2)
+        Validators.minLength(2),
+        Validators.pattern("[א-ת]+")
       ]),
-      'lastName': new FormControl(this.user.lastName, [
+      'lastname': new FormControl(this.user.lastName, [
         Validators.required
-      ]) 
+      ]),
+      'email': new FormControl(this.user.email, [
+        Validators.required,
+        Validators.email
+      ]),
+      'engfname': new FormControl(this.user.engFname, [
+        Validators.pattern("[a-zA-Z ]*")
+      ]),
+      'phone' : new FormControl("", [
+        Validators.pattern("[0-9]*"),
+        Validators.minLength(8),
+        Validators.maxLength(11)
+      ])
     });
   }
 
-  get firstname() { return this.userform.get('firstName'); }
-  get lastname() { return this.userform.get('lastName'); }
+  get firstname() { return this.userform.get('firstname'); }
+  get lastname() { return this.userform.get('lastname'); }
+  get email() { return this.userform.get('email'); }
+  get engfname() { return this.userform.get('engfname'); }
+  get phone() { return this.userform.get('phone'); }
 
 
   submitted = false;
--- RegistrationWebApp/src/app/user.ts
+++ RegistrationWebApp/src/app/user.ts
@@ -1,15 +1,16 @@
 export class User {
     constructor(
         public type : string,
-        public firstName : string,
-        public lastName : string,
-        public email : string,
-        public password : string,
-        public birthday : string,
+        public firstName? : string,
+        public lastName? : string,
+        public email? : string,
+        public password? : string,
+        public birthday? : string,
         public engFname? : string,
         public engLname? : string,
         public address? : string,
         public schoolName? : string,
         public uid? : string,
+        public phone? : string
 ){}
 }
