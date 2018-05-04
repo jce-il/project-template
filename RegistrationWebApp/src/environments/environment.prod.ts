4b2e2bb75162c5b6a6daeb9dfa3a781594234ba0
alexlvz <termister2000@gmail.com>
2018-05-04 13:29:59 +0300

    added production values

--- RegistrationWebApp/src/environments/environment.prod.ts
+++ RegistrationWebApp/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyC35sA7CVPczbN6GOnwUBnnx2ldnlmP3dw",
+    authDomain: "scientistscompetition.firebaseapp.com",
+    databaseURL: "https://scientistscompetition.firebaseio.com",
+    projectId: "scientistscompetition",
+    storageBucket: "scientistscompetition.appspot.com",
+    messagingSenderId: "66229167207"
+  }
 };
