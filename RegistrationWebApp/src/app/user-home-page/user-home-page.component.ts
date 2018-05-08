ac204b173cf1fb403e333bbffbce550d92c9104c
Adi <didah3@gmail.com>
2018-05-08 19:46:20 +0300

    user home page

--- RegistrationWebApp/src/app/user-home-page/user-home-page.component.ts
+++ RegistrationWebApp/src/app/user-home-page/user-home-page.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { DatabaseService } from '../services/database.service';
+import { AuthService } from '../services/auth.service';
+import { RouterLink, Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-home-page',
@@ -8,12 +10,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserHomePageComponent implements OnInit {
 
-  constructor() { }
+  constructor(public db: DatabaseService, public auth: AuthService, public router: Router) { }
 
   ngOnInit() {
-
+    this.db.registeredUsers = ''; //clean string - this is temp info only
+    this.db.getLoggedInUser(); // in order to print logged in user info - on init get it
   }
 
-
+//on update info button click updates logged in users info according to the info that was inserted in the temporary update form
+public updateInfo() {
+  this.db.updateListing(this.db.loggedInUser.email);
+  alert("Data updated!");
+}
 
 }
