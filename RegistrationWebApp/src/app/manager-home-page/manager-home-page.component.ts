52a54c719926d653bd704d2846a0f1c01eb23b9a
Itay-Hefetz <hefetz99@gmail.com>
2018-06-05 19:04:06 +0300

    manager

--- RegistrationWebApp/src/app/manager-home-page/manager-home-page.component.ts
+++ RegistrationWebApp/src/app/manager-home-page/manager-home-page.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ExcelService } from '../services/excel.service';
 import { DatabaseService } from '../services/database.service';
 import { RouterLink, Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
+
 
 @Component({
   selector: 'app-manager-home-page',
@@ -12,7 +14,7 @@ export class ManagerHomePageComponent implements OnInit {
 
   user_exp = [];
   proj_exp = [];
-  constructor(public excelService: ExcelService, public db: DatabaseService, public router: Router) { }
+  constructor(public excelService: ExcelService, public db: DatabaseService, public router: Router, private cookieService: CookieService) { }
 
   ngOnInit() {
     this.db.setMetaData();
@@ -38,5 +40,9 @@ export class ManagerHomePageComponent implements OnInit {
     this.router.navigate(['tablePage']);
   }
 
+  public go_to_reg(status){
+    this.cookieService.set('mode', status);
+    this.router.navigate(['registrationForm']);
+  }
 }
 
