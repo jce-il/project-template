d3ca43063cc5926af97590b3931e0e3071e8cc28
Adi <didah3@gmail.com>
2018-06-06 17:08:05 +0300

    clock

--- RegistrationWebApp/src/app/user-home-page/user-home-page.component.ts
+++ RegistrationWebApp/src/app/user-home-page/user-home-page.component.ts
@@ -116,28 +116,23 @@ public days(){
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
   var hours = Math.floor ((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);  
 
   $("#jours").html(days);
   $("#heures").html(hours);
   $("#minutes").html(minutes);
-  $("#secondes").html(seconds);
   
   // On stylise les éléments
   $("#jours").css("background-color",'rgba(126,85,39,0.2)');
   $("#heures").css("background-color",'rgba(126,85,39,0.2)');
   $("#minutes").css("background-color",'rgba(126,85,39,0.2)');
-  $("#secondes").css("background-color",'rgba(126,85,39,0.2)');
   
  $("jours").css("color", 'rgb(126,85,39)');
  $("heures").css("color",'rgb(126,85,39)');
  $("minutes").css("color",'rgb(126,85,39)');
-  $("secondes").css("color",'rgb(126,85,39)');
   
   $("jours").css("font-weight",'bold');
   $("heures").css("font-weight",'bold');
   $("minutes").css("font-weight",'bold');
-  $("secondes").css("font-weight",'bold');
 
   if (distance < 0) {
     clearInterval(x);
