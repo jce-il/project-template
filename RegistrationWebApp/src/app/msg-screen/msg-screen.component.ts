969408e945ed50d07819444e1629b252687b8d4c
alexlvz <37401545+alexlvz@users.noreply.github.com>
2018-06-11 12:07:12 +0300

    added alert before message deletion

--- RegistrationWebApp/src/app/msg-screen/msg-screen.component.ts
+++ RegistrationWebApp/src/app/msg-screen/msg-screen.component.ts
@@ -18,41 +18,37 @@ export class MsgScreenComponent implements OnInit {
   ngOnInit() {
     this.db.loggedInUserUID = this.cookieService.get('User uid');
     this.db.loggedIn = this.cookieService.get('User login status');
-    this.db.getLoggedInUser().then(()=> {
+    this.db.getLoggedInUser().then(() => {
       this.numOfMessage = this.db.loggedInUser.messages.length;
       this.downloadMsgs();
     })
   }
 
-  constructor(public db: DatabaseService,private cookieService: CookieService, public router: Router) { }
+  constructor(public db: DatabaseService, private cookieService: CookieService, public router: Router) { }
 
-  downloadMsgs()
-  {
-    for(var i = 0,j=this.numOfMessage-1;
-      i<this.db.loggedInUser.messages.length;i++,j--)
-    {
+  downloadMsgs() {
+    for (var i = 0, j = this.numOfMessage - 1;
+      i < this.db.loggedInUser.messages.length; i++ , j--) {
       this.msgArray[i] = this.db.loggedInUser.messages[j];
     }
   }
 
-  delteMessage(msg){
-    console.log(msg)
-    var i=this.db.loggedInUser.messages.length-1-msg;
-    console.log(this.db.loggedInUser.messages)
-    console.log(this.db.loggedInUser.messages[i])
-    this.numOfMessage--;
-    this.msgArray.slice(msg,1)
-    this.db.loggedInUser.messages.splice(i,1);
-    console.log(this.db.loggedInUser.messages)
-    this.db.user = this.db.loggedInUser;
+  delteMessage(msg) {
+    if (window.confirm('האם למחוק את ההודעה?')) {
+      var i = this.db.loggedInUser.messages.length - 1 - msg;
+      this.numOfMessage--;
+      this.msgArray.slice(msg, 1)
+      this.db.loggedInUser.messages.splice(i, 1);
+      this.db.user = this.db.loggedInUser;
       this.db.updateListing(this.db.loggedInUser.email);
       this.downloadMsgs()
+    }
   }
 
-  reply(message){
+  reply(message) {
     if (this.db.loggedInUser.type != 'מנהל')
-        this.router.navigate(['contactUs']);
-    else{
+      this.router.navigate(['contactUs']);
+    else {
       //add cookie
       this.router.navigate(['contactUs']);
     }
