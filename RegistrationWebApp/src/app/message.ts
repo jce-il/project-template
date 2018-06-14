bb26c6f98474d6a9bd6f21262aac9614805a2006
igor <thejaguar156@gmail.com>
2018-06-14 19:08:09 +0300

    date

--- RegistrationWebApp/src/app/message.ts
+++ RegistrationWebApp/src/app/message.ts
@@ -2,7 +2,7 @@ export class Message {
 
     constructor(public subj?: string,
                 public content?: string,
-                public date?: string, 
+                public date?: Date, 
                 public email?: string,
                 public name?: string,
                 public last_name?: string){}
